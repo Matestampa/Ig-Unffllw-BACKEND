6d43a89f28bb65520b9f8945a2dca2ca49812027
Matestampa <paredpiso5@gmail.com>
2024-03-01 19:44:44 -0300

    Added env vars to app_config

--- src/config/app_config.js
+++ src/config/app_config.js
@@ -1,9 +1,30 @@
-const CONFIG_VARS={
-    allow_requests:true
+const dotenv=require("dotenv");
+const {join}=require("path")
+
+let env_absPath=join(__dirname,"../../.env")
+dotenv.config({path:env_absPath});
+
+
+//Variables generales
+const APP_GEN_VARS={
+    dev_mode:process.env.ENV=="dev" ? true : false,
+}
+
+//Variables de conexion
+const APP_CONN_VARS={
+    host:process.env.HOST,
+    port:process.env.PORT,
+}
+
+//Variables de acceso
+const APP_ACCESS_VARS={
+    allow_requests:true,
+    exclusive_req_pwd:process.env.EXCL_REQ_PWD
 }
 
 function disable_requests(){
-    CONFIG_VARS.allow_requests=false;
+    APP_ACCESS_VARS.allow_requests=false;
 }
 
-module.exports={CONFIG_VARS,disable_requests};
+module.exports={APP_GEN_VARS,APP_CONN_VARS,
+               APP_ACCESS_VARS,disable_requests};
