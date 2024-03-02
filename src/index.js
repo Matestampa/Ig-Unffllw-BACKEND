41c6662a46b7a77ad48d2bceca7c935220490b19
Matestampa <paredpiso5@gmail.com>
2024-03-01 22:09:19 -0300

    Integrated conn vars

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const {App}=require("./app.js");
+const {APP_CONN_VARS}=require("./config/app_config.js");
 
-const PORT=3000;
+
+const PORT=APP_CONN_VARS.port;
 
 App.listen(PORT,()=>{console.log("Running oN port",PORT)});
 
