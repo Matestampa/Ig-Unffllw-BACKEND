c286442a9037c85cec0e6621f312d31fab65287c
Matestampa <paredpiso5@gmail.com>
2024-03-05 22:11:25 -0300

    Some deletes

--- src/services/Get_Followers/index.js
+++ src/services/Get_Followers/index.js
@@ -26,7 +26,7 @@ console.log(AccountsManager);*/
 const MAX_FOLLOWERS=2000;
 
 async function get_userInfo(username){
-    console.log("hii")
+    
     let AccountsManager=get_IgAccountsManager();
     
     //Cuenta para hacer la request.
@@ -41,7 +41,6 @@ async function get_userInfo(username){
     let user_info;
     
     try{
-        throw new Error("tu viejaaa");
         user_info=await userInfo_igRequest(username,req_account.authData.cookies,
                                            req_account.authData.headers,req_account.proxyData);
     }
@@ -75,7 +74,7 @@ const MS_BTW_REQ=200; //Milisecs entre requests a ig.
 
 //Return {followers:{user_id:username...}}
 async function get_followers(user_id,last_cursor){
-    console.log("nananan"); 
+    
     let AccountsManager=get_IgAccountsManager();
 
     let followers={};
