3edcd5011ecfb41a67c255a881456e3b0f86bd17
Matestampa <paredpiso5@gmail.com>
2024-03-15 22:43:52 -0300

    Support different envs

--- src/config/app_config.js
+++ src/config/app_config.js
@@ -1,7 +1,7 @@
 const dotenv=require("dotenv");
 const {join}=require("path")
 
-let env_absPath=join(__dirname,"../../.env")
+let env_absPath=join(__dirname,`../../.env.${process.env.APP_ENV}`);
 dotenv.config({path:env_absPath});
 
 
