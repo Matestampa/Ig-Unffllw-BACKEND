4f1369f0f77b1983057413219c25c793eae754cc
Matestampa <paredpiso5@gmail.com>
2024-04-11 20:15:18 -0300

    Added error handling to redis

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,23 +1,50 @@
 const {createClient}=require("redis");
 const {REDIS_CONN_VARS}=require("../config/redis_config.js");
 
-//Iniciamos client de redis
+const {internalError_handler,InternalError}=require("../error_handling");
+
+//Crear client de redis
 let redisClient=createClient({
-    url:`redis://${REDIS_CONN_VARS.host}:${REDIS_CONN_VARS.port}`
+    url:`redis://${REDIS_CONN_VARS.host}:${REDIS_CONN_VARS.port}`,
+    socket:{
+        reconnectStrategy:false
+    }
 })
 
+//------------------------- Error handling -----------------------------
 
+//Custom redis error
+class RedisError extends InternalError{
+    constructor(message,attachedError){
+        super(message,attachedError);
+        this.critic=true;
+    }
+}
+
+//General error handler
+redisClient.on("error",async (e)=>{
+    
+    //Si es error de conexion
+    if (e.code=="ECONNREFUSED"){
+        internalError_handler(new RedisError("Connection failed",e))
+    }
+    //Otro
+    else{
+        internalError_handler(new RedisError("",e))
+        await redisClient.disconnect();
+    }
+});
+
+
+//------------------------- Connection ---------------------------------------
 async function connect(){
     try{
         await redisClient.connect();
         console.log("Redis connection established");
     }
+    catch(e){}
+} 
 
-    catch(e){
-        console.log("Redis conection failed");
-        throw e;
-    }
-}
 
 connect();
 
