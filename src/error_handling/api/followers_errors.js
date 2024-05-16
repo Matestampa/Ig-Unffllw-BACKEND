ef5aa62c8b18c0715be28f76c67659f69cfad75b
Matestampa <paredpiso5@gmail.com>
2024-05-15 21:38:22 -0300

    Added specific api errors for "get followers"

---
+++ src/error_handling/api/followers_errors.js
@@ -0,0 +1,50 @@
+const {Api_ServerError,Api_BadRequest_Error}=require("./general_errors.js");
+
+
+class Api_PRIVATE_ACCOUNT_Error extends Api_BadRequest_Error{
+    constructor(message,data){
+        super(message,data);
+        
+        this.sub_code="PRIVATE_ACCOUNT";
+        this.message="The account is private";
+
+    }
+}
+
+class Api_FOLL_EXCESS_Error extends Api_BadRequest_Error{
+    constructor(message,data){
+        super(message,data);
+
+        this.sub_code="FOLL_EXCESS";
+        this.message="Followers limit excedeed";
+    }
+}
+
+class Api_NOMORE_REQ_Error extends Api_BadRequest_Error{
+    constructor(message,data){
+        super(message,data);
+
+        this.sub_code="NOMORE_REQ";
+        this.message="Requests limit excedeed";
+    }
+}
+
+class Api_REQUESTS_DISABLED_Error extends Api_ServerError{
+    constructor(message,data){
+        super(message,data);
+
+        this.sub_code="REQUESTS_DISABLED";
+        this.message="The server is down, please try later";
+    }
+}
+
+const FOLLOWERS_ERRORS={
+    PRIVATE_ACCOUNT: (message,data)=>new Api_PRIVATE_ACCOUNT_Error(message,data),
+    FOLL_EXCESS: (message,data)=>new Api_FOLL_EXCESS_Error(message,data),
+    NOMORE_REQ: (message,data)=>new Api_NOMORE_REQ_Error(message,data),
+    REQUESTS_DISABLED: (message,data)=>new Api_REQUESTS_DISABLED_Error(message,data)
+}
+
+module.exports={
+    FOLLOWERS_ERRORS
+}
--- src/error_handling/index.js
+++ src/error_handling/index.js
@@ -1,6 +1,6 @@
-const {apiError_handler,Error4User,DEF_API_ERRORS}=require("./api");
+const {apiError_handler,Error4User,DEF_API_ERRORS,FOLLOWERS_ERRORS}=require("./api");
 const {internalError_handler,InternalError,GEN_INT_ERRORS}=require("./internal");
 
 
-module.exports={apiError_handler,DEF_API_ERRORS,internalError_handler,
-                Error4User,InternalError,GEN_INT_ERRORS};
+module.exports={apiError_handler,DEF_API_ERRORS,FOLLOWERS_ERRORS,
+                internalError_handler,InternalError,GEN_INT_ERRORS};
