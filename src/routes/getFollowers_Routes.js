912315ea7bb1a12b599c45459ada29cefe17441b
Matestampa <paredpiso5@gmail.com>
2024-06-12 21:29:24 -0300

    Added validator for get followers endpoint

--- src/routes/getFollowers_Routes.js
+++ src/routes/getFollowers_Routes.js
@@ -3,9 +3,12 @@ const router=express.Router();
 
 const Controller=require("../controllers/getFollowers_Controller.js");
 
+const Validator=require("../validators/getFollowers_validator.js");
+
+
 router.get("/user_info/:username",Controller.user_info);
 
-router.post("/nexts",Controller.next_followers);
+router.post("/nexts",Validator.validate_nextFoll,Controller.next_followers);
 
 
 module.exports=router;
